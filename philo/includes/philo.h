3f933afb2b5cc95ef93ae656584be234109c0378
Samantha LIMON <salimon@e3r11p2.clusters.42paris.fr>
2022-10-16 21:56:14 +0200

    heh

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -6,29 +6,29 @@
 /*   By: salimon <salimon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/22 10:20:36 by salimon           #+#    #+#             */
-/*   Updated: 2022/09/26 04:47:35 by salimon          ###   ########.fr       */
+/*   Updated: 2022/10/16 21:55:23 by salimon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
-#define PHILO_H
+# define PHILO_H
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <pthread.h>
-#include <semaphore.h>
-#include <sys/time.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <pthread.h>
+# include <semaphore.h>
+# include <sys/time.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
 
 /*
 les int des forks correspondent aux emplacement
 des fourchettes dans mon tableau de forks
 */
-typedef struct	s_philosopher
+typedef struct s_philosopher
 {
 	pthread_t		id;
 	int				position;
@@ -55,16 +55,17 @@ typedef struct s_datas
 	long long		timestamp;
 	pthread_mutex_t	*forks;
 	pthread_mutex_t	logs;
-	pthread_mutex_t meal;
+	pthread_mutex_t	meal;
 	t_philosopher	*philos;
 }				t_datas;
 
-int 		error_management(t_datas *datas, unsigned int error_code);
+int			error_management(t_datas *datas, unsigned int error_code);
 int			parsing(t_datas *datas, int argc, char **argv);
 int			ft_atoi(const char *nb);
-long long	get_time();
-void		print_log(t_philosopher *philo, long long ms, int id, char *message);
-void   		eat(t_philosopher *philo);
+long long	get_time(void);
+void		print_log(
+				t_philosopher *philo, long long ms, int id, char *message);
+void		eat(t_philosopher *philo);
 int			check_dining_end(t_datas *datas);
 void		ft_clear(t_datas *datas);
 int			smart_sleep(t_datas *datas, long long ms);
